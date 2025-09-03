e1879fe3d846e674c43a4939134e49e8c7ce9edc
Rahul Gaur <rahul.nbg@gmail.com>
2025-09-03 21:50:01 +0400

    Add incremental org_id migrations and fix middleware stack

--- src/etlp_mapper/middlewares.clj
+++ src/etlp_mapper/middlewares.clj
@@ -2,3 +2,15 @@
   (:require
     [ring.middleware.cors :refer [wrap-cors]]
     [integrant.core :as ig]))
+
+(defn cors-middleware
+  "Allow cross-origin requests for common HTTP methods and headers."
+  [handler]
+  (wrap-cors handler
+             :access-control-allow-origin [#".*"]
+             :access-control-allow-methods [:get :post :put :delete :options]
+             :access-control-allow-headers ["Content-Type" "Authorization"]))
+
+(defmethod ig/init-key :etlp/middleware.cors
+  [_ _]
+  cors-middleware)
