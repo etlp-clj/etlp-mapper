99660faea3d44f7e79a550fb6e2771e698a4d306
Rahul Gaur <rahul.nbg@gmail.com>
2026-03-31 20:20:59 +0400

    fix: prompt tuning

--- src/etlp_mapper/middlewares.clj
+++ src/etlp_mapper/middlewares.clj
@@ -13,24 +13,41 @@
              :access-control-allow-origin [#".*"]
              :access-control-allow-methods [:get :post :put :delete :options]
              :access-control-allow-headers ["Content-Type" "Authorization"]))
+
+(def allowed-origins
+  #{"http://localhost:5173"
+    "http://127.0.0.1:5173"
+    "http://192.168.1.21:5173"})
+
+(defn- allowed-origin?
+  [origin]
+  (contains? allowed-origins origin))
+
+(defn- cors-headers
+  [origin]
+  {"Access-Control-Allow-Origin"      origin
+   "Access-Control-Allow-Methods"     "GET, POST, PUT, DELETE, OPTIONS"
+   "Access-Control-Allow-Headers"     "Content-Type, Authorization"
+   "Access-Control-Allow-Credentials" "true"
+   "Access-Control-Expose-Headers"    "Location"
+   "Vary"                             "Origin"})
+
 (defn cors-middleware
   "Middleware that allows CORS requests and handles preflight OPTIONS."
   [handler]
   (fn [request]
-    (if (= :options (:request-method request))
-      {:status  200
-       :headers {"Access-Control-Allow-Origin" "http://localhost:5173"
-                 "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS"
-                 "Access-Control-Allow-Credentials" "true"
-                 "Access-Control-Allow-Headers" "Content-Type, Authorization"}
-       :body    "OK"}
-      (let [response (handler request)]
-        (-> response
-            (assoc-in [:headers "Access-Control-Allow-Origin"] "http://localhost:5173")
-            (assoc-in [:headers "Access-Control-Allow-Methods"] "GET, POST, PUT, DELETE, OPTIONS")
-            (assoc-in [:headers "Access-Control-Allow-Headers"] "Content-Type, Authorization")
-            (assoc-in [:headers "Access-Control-Allow-Credentials"] "true")
-            (assoc-in [:headers "Access-Control-Expose-Headers"] "Location"))))))
+    (let [origin (get-in request [:headers "origin"])]
+      (if (= :options (:request-method request))
+        (if (allowed-origin? origin)
+          {:status  200
+           :headers (cors-headers origin)
+           :body    "OK"}
+          {:status 403
+           :body "CORS origin not allowed"})
+        (let [response (handler request)]
+          (if (allowed-origin? origin)
+            (update response :headers merge (cors-headers origin))
+            response))))))
 
 (defmethod ig/init-key :etlp-mapper.middlewares/cors
   [_ _]
